60d2ccb68ac9eebf3e7afbb9a88818ac905dc74d
Name Ping Louie <namepinglouie@gmail.com>
2022-06-29 12:21:23 -0400

    styled navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,8 @@ import {Link} from "react-router-dom";
 export default function Navbar() {
     return(
         <nav id="navigation">
-            <p className="nav-title">&#123; Restaurant App &#125;</p>
-            <div>
-                <Link to = "/">HOME</Link>
+            <p className="nav-title">&#123; <Link to = "/">Restaurant App</Link> &#125;</p>
+            <div id= "nav-links">
                 <Link to = "/restaurants">RESTAURANTS</Link>
                 <Link to = "/restaurants/new">NEW RESTAURANT</Link>
                 <Link to = "/reservations">RESERVATIONS</Link>
